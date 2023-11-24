1502c45483670358a26c8f5bcdb44471840b10b7
Sithu Aung Khant <sithu.sithuthedev@gmail.com>
2023-11-24 21:11:33 +0630

    Refactor input element placement and update function exit conditions.

--- index.js
+++ index.js
@@ -2,14 +2,10 @@
 const year_result = document.getElementById('year');
 const month_result = document.getElementById('month');
 const day_result = document.getElementById('day');
-// Input Elements
-const year_input = document.getElementById('yy');
-const month_input = document.getElementById('mm');
-const day_input = document.getElementById('dd');
 
 const labels = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('input');
-const error_messages = document.querySelectorAll('error-message');
+const error_messages = document.querySelectorAll('.error-message');
 const submit_btn = document.getElementById('submit');
 
 // Used text input instead of number for flexible customization.
@@ -21,6 +17,7 @@ function restrictInput(input) {
 function validateAndHandleError(value, index, errorMessage, maxValue) {
   const errorText =
     value.length === 0 ? 'This field is required' : errorMessage;
+
   if (value.length === 0 || (maxValue !== undefined && value > maxValue)) {
     error_messages[index].innerHTML = errorText;
     error_messages[index].classList.add('error');
@@ -28,36 +25,53 @@ function validateAndHandleError(value, index, errorMessage, maxValue) {
     inputs[index].classList.add('error');
     return false; // Indicates validation failure
   }
+  error_messages[index].innerHTML = '';
+  error_messages[index].classList.remove('error');
+  labels[index].classList.remove('error');
+  inputs[index].classList.remove('error');
+
   return true; // Indicates validation success
 }
 
 function calculateAge() {
-  // Validate Day
-  validateAndHandleError(day_input, 0, 'Must be a valid date', 31);
+  // Input Elements
+  const year_input = document.getElementById('yy').value;
+  const month_input = document.getElementById('mm').value;
+  const day_input = document.getElementById('dd').value;
 
-  // Validation for Month
-  validateAndHandleError(month_input, 1, 'Must be a valid month', 12);
-
-  // Validation for Year
-  validateAndHandleError(
+  // Validating Date, Monnth & Year
+  const validate_day = validateAndHandleError(
+    day_input,
+    0,
+    'Must be a valid date',
+    31
+  );
+  const validate_month = validateAndHandleError(
+    month_input,
+    1,
+    'Must be a valid month',
+    12
+  );
+  const validate_year = validateAndHandleError(
     year_input,
     2,
     'Must be in the past',
     new Date().getFullYear()
   );
 
-  let dob = new Date(
-    `${month_input.value}/ ${day_input.value}/ ${year_input.value}`
-  );
-  let ageDiff = Date.now() - dob;
-  let ageDate = new Date(ageDiff);
-  let ageYears = ageDate.getUTCFullYear() - 1970;
-  let ageMonth = ageDate.getUTCMonth();
-  let ageDay = ageDate.getUTCDate();
+  if (validate_day && validate_month && validate_year) {
+    let dob = new Date(`${month_input}/ ${day_input}/ ${year_input}`);
+    let ageDiff = Date.now() - dob;
+    let ageDate = new Date(ageDiff);
+    let ageYears = ageDate.getUTCFullYear() - 1970;
+    let ageMonth = ageDate.getUTCMonth();
+    let ageDay = ageDate.getUTCDate();
 
-  year_result.textContent = ageYears;
-  month_result.textContent = ageMonth;
-  day_result.textContent = ageDay;
+    year_result.textContent = ageYears;
+    month_result.textContent = ageMonth;
+    day_result.textContent = ageDay;
+  }
+  return;
 }
 
 submit_btn.addEventListener('click', (e) => {
@@ -65,8 +79,10 @@ submit_btn.addEventListener('click', (e) => {
   calculateAge();
 });
 
-year_input.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
-    calculateAge();
-  }
+inputs.forEach((input) => {
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      calculateAge();
+    }
+  });
 });
